627dc9bec4e4fb7a0771216321feeb5d9a54d504
Jettsyy <teazyyy2512@gmail.com>
2025-02-21 10:00:50 +0700

    feat: base react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import { useRoutes } from "react-router-dom";
+import "./App.css";
+import List from "./components/list";
 
 function App() {
+  {
+    /* Viết router ở đây như ví dụ bên dưới */
+  }
+  const routes = useRoutes([{ path: "/books", element: <List /> }]);
+  return routes;
 
-  return (
-    <>
-     {/* Viết router ở đây */}
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
